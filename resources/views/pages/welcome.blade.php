f2dd0a516cde020d34c2fd19d2ebfeffbf853c91
dimonenkov <dimo.nenkov@me.com>
2018-02-18 20:40:41 +0200

    Part 21

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -14,39 +14,21 @@
       </div> <!-- end of header .row -->
 
       <div class="row"> 
-        <div class="col-md-8">
-          <div class="post">
-            <h3>Post Title</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga quia soluta architecto fugit tempore eveniet at, distinctio repellendus dolorem rerum repellat, corporis quam, placeat eos amet ipsum quas sapiente expedita.</p>
-            <a href="#" class="btn btn-primary">Read More</a>
-          </div>
+        <div class="col-md-8"> 
+
+          @foreach ($posts as $post)
+
+            <div class="post">
+              <h3>{{ $post->title }}</h3>
+              <p>{{ substr($post->body, 0, 300) }}{{ strlen($post->body) > 300 ? "..." : "" }}</p>
+              <a href="#" class="btn btn-primary">Read More</a>
+            </div>
 
           <hr>
 
-          <div class="post">
-              <h3>Post Title</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga quia soluta architecto fugit tempore eveniet at, distinctio repellendus dolorem rerum repellat, corporis quam, placeat eos amet ipsum quas sapiente expedita.</p>
-              <a href="#" class="btn btn-primary">Read More</a>
-            </div>
-
-            <hr>
-
-            <div class="post">
-              <h3>Post Title</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga quia soluta architecto fugit tempore eveniet at, distinctio repellendus dolorem rerum repellat, corporis quam, placeat eos amet ipsum quas sapiente expedita.</p>
-              <a href="#" class="btn btn-primary">Read More</a>
-            </div>
-
-            <hr>
-
-            <div class="post">
-              <h3>Post Title</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga quia soluta architecto fugit tempore eveniet at, distinctio repellendus dolorem rerum repellat, corporis quam, placeat eos amet ipsum quas sapiente expedita.</p>
-              <a href="#" class="btn btn-primary">Read More</a>
-            </div>
+          @endforeach
 
         </div>
-          
 
         <div class="col-md-3 col-md-offset-1">
           <h2>Sidebar</h2>
