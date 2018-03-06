d5980fa1a70aa829c00e18d794bd17d774ab463d
dimonenkov <dimo.nenkov@me.com>
2018-03-07 01:12:58 +0200

    Part 25/2

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -6,7 +6,7 @@
 
 	
 	<div class="row">
-		<div class="col-md-12">
+		<div class="col-md-8 col-md-offset-2">
 			<h1>Blog</h1>
 		</div>
 	</div>
@@ -18,11 +18,19 @@
 			<h5>Published: {{ date('M j, Y', strtotime($post->created_at)) }}</h5>
 
 			<p>{{ substr($post->body, 0, 250) }} {{ strlen($post->body) > 250 ? '...' : ""}}</p>
-			<a href="{{ route('blog.single', $post->id) }}">Read More</a>
-
+			<a href="{{ route('blog.single', $post->id) }}" class="btn btn-primary">Read More</a>
+			<hr>
 		</div>
 	</div>
 	@endforeach
+
+	<div class="row">
+		<div class="col-md-12">
+			<div class="text-center">
+				{!! $posts->links() !!}
+			</div>
+		</div>
+	</div>
 
 @endsection
 
